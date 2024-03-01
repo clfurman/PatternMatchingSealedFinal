095078b45456afb6473c6e349ed288052d80078c
clfurman <crystal.furman@gmail.com>
2024-03-01 09:51:51 -0500

    add DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 public class DogTester {
     public static void main (String [] args) {
-        Dog d = new Dog (new LicenseName ("Brady the Blue Brindle", "Brady"), "Whippet", 46.7);
-        Cat c = new Cat ("Sweetie", "Siamese", 7.5);
+        Dog brady = new Dog (new LicenseName ("Brady the Blue Brindle", "Brady"), "Whippet", 46.7);
+        Cat sweetie = new Cat ("Sweetie", "Siamese", 7.5);
         ArrayList <Animal> animals = new ArrayList<>();
-        animals.add(d);
-        animals.add(c);
+        animals.add(brady);
+        animals.add(sweetie);
 
         for (Animal a: animals){
             switch (a) {
-                case Dog (LicenseName n, _, _)->System.out.println(n.nickname());
+                case Dog (LicenseName name, _, _)->System.out.println(name.nickname());
                 
-                case Cat (String n, _, _)->System.out.println(n);
+                case Cat (String name, _, _)->System.out.println(name);
             } 
         }
     }
