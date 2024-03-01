94e75de9e48e36e2a8666a25791292c349e25b32
clfurman <crystal.furman@gmail.com>
2024-03-01 14:50:44 -0500

    add DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -1,25 +1,27 @@
+
 import java.util.*;
 
 public class DogTester {
-    public static void main (String [] args) {
+
+    public static void main(String[] args) {
         ArrayList<Animal> animals = new ArrayList<>();
 
-        System.out.println ("Let\'s simulate a vet office.");
-        Dog brady = new Dog (new LicenseName("Brady the Brindle", "Brady"), "Whippet", 39.6);
-        Cat mittens = new Cat ("Mittens", "Siamese", 8.2);
-        animals.add (brady);
-        animals.add (mittens);
-        System.out.println (animals);
+        System.out.println("Let\'s simulate a vet office.");
+        Dog brady = new Dog(new LicenseName("Brady the Brindle", "Brady"), "Whippet", 39.6);
+        Cat mittens = new Cat("Mittens", "Siamese", 8.2);
+        animals.add(brady);
+        animals.add(mittens);
+        System.out.println(animals);
 
         for (Animal a : animals) {
             switch (a) {
                 //since breed and weight are not being used, we don't need to store these values
-                case Dog (LicenseName name, _, _): {                
-                    System.out.println (name.showName());
+                case Dog(LicenseName name, _, _): {
+                    System.out.println(name.showName());
                     break;
                 }
-                case Cat (String name, _, _): {
-                    System.out.println (name);
+                case Cat(String name, _, _): {
+                    System.out.println(name);
                     break;
                 }
                 //since Animal is sealed, no default is needed. The only cases are Dog and Cat
@@ -32,16 +34,14 @@ public class DogTester {
 sealed interface Animal permits Dog, Cat {
 }
 
-record LicenseName (String showName, String nickName) {
-} 
+record LicenseName(String showName, String nickName) {
 
-record Dog (LicenseName name, String breed, double weight) implements Animal {
 }
 
-record Cat (String name, String breed, double weight) implements Animal {
+record Dog(LicenseName name, String breed, double weight) implements Animal {
+
 }
 
+record Cat(String name, String breed, double weight) implements Animal {
 
-
-
-
+}
