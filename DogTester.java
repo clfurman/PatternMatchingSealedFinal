54cd15ac97b0bb3d61e712289b2b16125eb906d6
clfurman <crystal.furman@gmail.com>
2024-03-01 14:48:34 -0500

    add DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -1,23 +1,24 @@
+
 import java.util.*;
 
 public class DogTester {
-    public static void main (String [] args) {
+
+    public static void main(String[] args) {
         ArrayList<Animal> animals = new ArrayList<>();
 
-        System.out.println ("Let\'s simulate a vet office.");
-        Dog brady = new Dog (new LicenseName("Brady the Brindle", "Brady"), "Whippet", 39.6);
-        Cat mittens = new Cat ("Mittens", "Siamese", 8.2);
-        animals.add (brady);
-        animals.add (mittens);
-        System.out.println (animals);
+        System.out.println("Let\'s simulate a vet office.");
+        Dog brady = new Dog(new LicenseName("Brady the Brindle", "Brady"), "Whippet", 39.6);
+        Cat mittens = new Cat("Mittens", "Siamese", 8.2);
+        animals.add(brady);
+        animals.add(mittens);
+        System.out.println(animals);
 
         for (Animal a : animals) {
             //Pattern matching used to declare and assign the variable dog to (Dog)a
             if (a instanceof Dog dog) {
-                System.out.println (dog.name().showName() + " is a " + dog.breed());
-            }
-            else if (a instanceof Cat cat) {
-                System.out.println (cat.name() + " is a " + cat.breed());
+                System.out.println(dog.name().showName() + " is a " + dog.breed());
+            } else if (a instanceof Cat cat) {
+                System.out.println(cat.name() + " is a " + cat.breed());
             }
         }
     }
@@ -27,15 +28,14 @@ public class DogTester {
 sealed interface Animal permits Dog, Cat {
 }
 
-record LicenseName (String showName, String nickName) {
-} 
+record LicenseName(String showName, String nickName) {
 
-record Dog (LicenseName name, String breed, double weight) implements Animal {
 }
 
-record Cat (String name, String breed, double weight) implements Animal {
+record Dog(LicenseName name, String breed, double weight) implements Animal {
+
 }
 
+record Cat(String name, String breed, double weight) implements Animal {
 
-
-
+}
