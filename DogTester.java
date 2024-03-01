d45e6fc297485592ac55c4e2d7cd23b4372d0196
clfurman <crystal.furman@gmail.com>
2024-03-01 09:42:18 -0500

    add DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -2,8 +2,8 @@
 public class DogTester {
     public static void main (String [] args) {
         System.out.println ("Let\'s simulate a vet office.");
-        Dog d = new Dog ("Brady", "Whippet", 39.6);
-        System.out.println (d);
+        Dog brady = new Dog ("Brady", "Whippet", 39.6);
+        System.out.println (brady);
     }
 }
 
